0451c013b673ffbeb51b0af154a586331d95a383
Brittany Ellich <brittanyellich@github.com>
2025-03-04 15:40:11 -0800

    Update seeds

--- backend/db/seed.rb
+++ backend/db/seed.rb
@@ -5,13 +5,13 @@ kids = DB[:kids]
 elliot = kids.insert(
   name: "Elliot",
   age: 8,
-  image_file: "elliot.png"
+  image_file: "basketball.jpg"
 )
 
 max = kids.insert(
   name: "Max",
   age: 3,
-  image_file: "max.png"
+  image_file: "unicorn.jpg"
 )
 
 routines = DB[:routines]
@@ -39,7 +39,7 @@ shoes = routine_tasks.insert(
   name: "Put on shoes. Yes, both of them.",
   routine_id: get_out_the_door,
   buffer_time: 1,
-  image_file: "shoes.png",
+  image_file: "shoes.jpg",
   audio_file: "shoes.png",
   kids_completed: "",
   status: "in_progress"
@@ -49,7 +49,7 @@ backpack = routine_tasks.insert(
   name: "Grab your backpack. No you can't bring bring the cat to school.",
   routine_id: get_out_the_door,
   buffer_time: 3,
-  image_file: "backpack.png",
+  image_file: "backpack.jpg",
   audio_file: "backpack.png",
   kids_completed: "",
   status: "in_progress"
@@ -59,7 +59,7 @@ umbrella = routine_tasks.insert(
   name: "It's going to rain, grab and umbrella.",
   routine_id: get_out_the_door,
   buffer_time: 3,
-  image_file: "umbrella.png",
+  image_file: "umbrella.jpg",
   audio_file: "umbrella.png",
   kids_completed: "",
   status: "in_progress"
@@ -68,7 +68,7 @@ umbrella = routine_tasks.insert(
 coat = routine_tasks.insert(
   name: "Put a coat on!",
   buffer_time: 3,
-  image_file: "coat.png",
+  image_file: "coat.jpg",
   audio_file: "coat.png",
   kids_completed: "",
   status: "in_progress"
@@ -77,7 +77,7 @@ coat = routine_tasks.insert(
 water_bottle = routine_tasks.insert(
   name: "Grab your water bottle. Is it full?",
   buffer_time: 3,
-  image_file: "water_bottle.png",
+  image_file: "water_bottle.jpg",
   audio_file: "water_bottle.png",
   kids_completed: "",
   status: "in_progress"
@@ -86,7 +86,7 @@ water_bottle = routine_tasks.insert(
 sunscreen = routine_tasks.insert(
   name: "Put on sunscreen. Hold still!",
   buffer_time: 3,
-  image_file: "sunscreen.png",
+  image_file: "sunscreen.jpg",
   audio_file: "sunscreen.png",
   kids_completed: "",
   status: "in_progress"
