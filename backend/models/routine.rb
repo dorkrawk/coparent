c78a895ee5fb4aaad038af80a1e953142eb5c926
Dave Schwantes <dave.schwantes@gmail.com>
2025-03-04 14:37:57 -0800

    ok the basic model functionality works

--- backend/models/routine.rb
+++ backend/models/routine.rb
@@ -1,6 +1,7 @@
 require 'sequel'
 DB_PATH = "./db/database.db"
 DB = Sequel.sqlite(DB_PATH)
+require 'json'
 
 require_relative "kid"
 require_relative "routine_task"
@@ -13,12 +14,26 @@ class Routine < Sequel::Model(:routines)
     complete: "complete"
   }
 
+  def response_values
+    response_object = values
+    response_object[:kids] = kids.map(&:values)
+    response_object[:kid_ids] = kid_ids
+    response_object[:routine_tasks] = filtered_routine_tasks.map(&:values)
+    response_object[:reward] = reward.values
+
+    response_object
+  end
+
   def routine_tasks
     RoutineTask.where(routine_id: id)
   end
 
   def filtered_routine_tasks
     routine_tasks
+  end
+
+  def check_and_update_status!
+
   end
 
   def reward
